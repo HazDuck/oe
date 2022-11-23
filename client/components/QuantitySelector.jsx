a4af8a09bbfdc444d4e30ed43afada0e011e2411
Pete Heyes <peter.e.heyes@gmail.com>
2022-11-23 18:30:19 +0000

    quantity selector component

---
+++ client/components/QuantitySelector.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+
+const QuantitySelector = () => {
+  return (
+    <div className='main__quantity'>
+      <span>Qty</span>
+      <div className='flex'>
+        <button></button>
+      </div>
+    </div>
+  ) 
+}
+
+export default QuantitySelector
