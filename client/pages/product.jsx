fa1ffa0533a2ac763504bfbb176d198a4eb9946d
Pete Heyes <peter.e.heyes@gmail.com>
2022-11-23 18:29:52 +0000

    implement utility styles and destructure data

--- client/pages/product.jsx
+++ client/pages/product.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { getProductData } from '.'
 import LoadingIcon from '../components/LoadingIcon'
+import QuantitySelector from '../components/QuantitySelector'
 
 const Product = () => {
-  const [productData, setProductData] = useState(null)
+  const [productData, setProductData] = useState({})
 
   useEffect(() => {
     (async function() {
@@ -12,11 +13,18 @@ const Product = () => {
       setProductData(data.allProducts[0]?.fields)
     })()
   }, [])
+
+  const {
+    img_url,
+    name,
+    power,
+    price
+  } = productData
   
   return (
     productData?.id ? (
       <div className='product'>
-        <header className='header'>
+        <header className='header flex space-between'>
           <div 
             className='header__logo'
             alt='Octopus Energy Logo'>
@@ -28,10 +36,16 @@ const Product = () => {
         </header>
         <main className='main'>
           <div className='main__image-container'>
-            <img src={productData.img_url} alt={`${productData.img_url} product image`} />
+            <img src={img_url} alt={`${name} product image`} />
           </div>
-          <div className='main__form'>
-            <h1 className='main__form-title'>{productData.name}</h1>
+          <div className='main__details'>
+            <h1 className='main__details-title'>{name}</h1>
+            {/* Packet of 4 should probably be returned with the product data */}
+            <p>{power} // Packet of 4</p>
+          </div>
+          <div className='flex space-between'>
+            <p className='main__price'>{price}</p>
+            <QuantitySelector />
           </div>
         </main>
         
