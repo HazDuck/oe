1a3fc373ca2cf6fc7f04a7b455d8f16d8d6f0091
Pete Heyes <peter.e.heyes@gmail.com>
2022-11-27 15:52:44 +0000

    Notes and comments

--- client/test/product.test.js
+++ client/test/product.test.js
@@ -1,7 +1,17 @@
 import { render, fireEvent } from "@testing-library/react";
+import CartBadge from "../components/CartBadge";
 import QuantitySelector from "../components/QuantitySelector";
 
 test("should be able to increase and decrease product quantity", async () => {
+
+  // My understanding is that because I am handling state via context api I my tests will fail (currently the test is failing on the .toHaveTextConent 
+  // on anything that gets passed through state e.g. productCount. 
+  // After some googling I believe I would need to mock both the data call and also using context. 
+
+  // Upon reflection I'd take a much more TDD approach, rather than building first and testing second. 
+
+  // Attempted to work out how to mock this but ran out of time 👇
+
   // jest.mock('../context', () => ({
   //   useProduct: () => ({
   //     productData: {},
@@ -31,7 +41,7 @@ test("should be able to increase and decrease product quantity", async () => {
 });
 
 test("should be able to add items to the basket", async () => {
-  // const { getByText, getByTitle } = render(<Product />);
+  //this test spans x3 components so this test would need to reflect this.
   const { getByText, getByTitle } = render(<QuantitySelector />);
 
   const increaseQuantity = getByText("+");
